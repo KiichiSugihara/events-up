91ef33d396b36ce3224af70f70b22bfb64cc324e
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-02-16 22:34:29 +0900

    add server guard

--- server/server.js
+++ server/server.js
@@ -4,10 +4,34 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = 8000;
+const jwt = require('express-jwt'); // NEW
+const jwksRsa = require('jwks-rsa');
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+
+// Set up Auth0 configuration
+const authConfig = {
+  domain: 'dev-tyq151jm.auth0.com',
+  audience: 'https://vue-express-api.com'
+};
+
+// Create middleware to validate the JWT using express-jwt
+const checkJwt = jwt({
+  // Provide a signing key based on the key identifier in the header and the signing keys provided by your Auth0 JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://dev-tyq151jm.auth0.com/.well-known/jwks.json'
+  }),
+
+  // Validate the audience (Identifier) and the issuer (Domain).
+  audience: 'https://vue-express-api.com',
+  issuer: 'https://dev-tyq151jm.auth0.com/',
+  algorithms: ['RS256']
+});
 
 // mock data to send to our frontend
 let events = [
@@ -50,7 +74,7 @@ app.get('/events', (req, res) => {
   res.send(events);
 });
 
-app.get('/events/:id', (req, res) => {
+app.get('/events/:id', checkJwt, (req, res) => {
   const id = Number(req.params.id);
   const event = events.find(event => event.id === id);
   res.send(event);
